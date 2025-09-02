2d9ea2cac6c9aa9948dd45058545234e5cf5d5cb
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2025-09-02 15:04:32 -0300

    comentarios

---
+++ Trabalho02SD/src/trabalho02/Master.java
@@ -0,0 +1,158 @@
+package trabalho02;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.net.*;
+
+public class Master {
+    private static final int MULTICAST_PORT = 8000;
+    private static final String MULTICAST_GROUP = "228.5.6.7";
+    private static final int TIMEOUT = 5000;
+    private static final int SYNC_INTERVAL = 10000;
+
+	private int horario;
+	private int numeroSlaves;
+	private Map<InetSocketAddress, Integer> listaDifSlaves;
+	
+	public Master(int horario, int numeroSlaves) {
+		this.horario = horario;
+		this.numeroSlaves = numeroSlaves;
+		listaDifSlaves = new ConcurrentHashMap<>();
+	}
+
+    public void enviarRequest() {
+        try {
+        	// Criando comunicação em grupo
+            MulticastSocket socket = criarMulticastSocket();
+            InetAddress group = InetAddress.getByName(MULTICAST_GROUP);
+            socket.joinGroup(group);
+            System.out.println("Master iniciado, clock: " + horario);
+
+            // A cada 10s o master repete e processo e envia o horário para os slaves, recebendo as diferenças
+            while (true) {
+                enviarHorario(socket, group);
+                receberDiferencaHorario(socket);
+                Thread.sleep(SYNC_INTERVAL);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MulticastSocket criarMulticastSocket() throws Exception {
+        MulticastSocket socket = new MulticastSocket(MULTICAST_PORT);
+        socket.setSoTimeout(TIMEOUT);
+        return socket;
+    }
+
+    // O master envia o próprio horário para todos os slaves
+    private void enviarHorario(MulticastSocket socket, InetAddress group) throws Exception {
+        String msg = "HORARIO_MASTER:" + horario;
+        NetworkUtils.sendMulticastPacket(socket, msg.getBytes(), group, MULTICAST_PORT);
+        System.out.println("Master enviou horário: " + horario);
+    }
+
+    private void receberDiferencaHorario(MulticastSocket socket) throws Exception {
+        listaDifSlaves.clear();
+        int recebidos = 0;
+
+        while (recebidos < numeroSlaves) {
+            try {
+            	// Master recebe os pacotes dos slaves
+                DatagramPacket packet = NetworkUtils.receivePacket(socket);
+                String message = new String(packet.getData(), 0, packet.getLength());
+                
+                // Master também recebe a própria resposta, então se a mensagem recebida for dele mesmo, ela é ignorada
+                // Ou seja, só processa as mensagens recebidas pelos slaves
+                if (!message.startsWith("HORARIO_MASTER")) {
+                    processarDiferencasSlave(packet, message);
+                    recebidos++;
+                }
+            } catch (SocketTimeoutException e) {
+                if (listaDifSlaves.size() >= numeroSlaves) {
+                    break;
+                }
+            }
+        }
+
+        // Se a lista não estiver vazia, o master calcula a média dos horários
+        if (!listaDifSlaves.isEmpty()) {
+            sincronizarHorarios(socket);
+        }
+    }
+
+    private void processarDiferencasSlave(DatagramPacket packet, String message) {
+    	// A mensagem recebida é dividida em partes 
+        String[] parts = message.split(":");
+        if (parts.length == 4 && parts[0].equals("DIF")) {
+
+            int diff = Integer.parseInt(parts[1]);
+            String ip = parts[2];
+            int port = Integer.parseInt(parts[3]);
+            
+            // O endereço e a diferença de horário recebida dos slaves é armazenada na listaDifSlaves 
+            InetSocketAddress address = new InetSocketAddress(ip, port);
+            listaDifSlaves.put(address, diff);
+            System.out.println("Recebeu diferença " + diff + " do Slave em " + address);
+        }
+    }
+
+    private void sincronizarHorarios(MulticastSocket socket) throws Exception {
+    	// O Master adiciona sua própria diferença na listaDifSlaves para calcular a média
+        listaDifSlaves.put(new InetSocketAddress("localhost", MULTICAST_PORT), 0);
+        System.out.println("Incluída diferença do Master: 0");
+
+        int avgDiff = calcularDiferencaMedia(listaDifSlaves);
+        
+        // O Master calcula e aplica o ajuste no prórpio horário
+        
+        /* COMENTÁRIO ALANA -> O MÉTODO calcularAjusteMaster PARECE INÚTIL, JÁ QUE ELE SÓ RETORNA O QUE JÁ PASSAMOS COMO PARÂMETRO
+         * FORMA ANTIGA:
+         * int masterAdjustment = calcularAjusteMaster(avgDiff);
+         * aplicarAjustesMaster(masterAdjustment);*/
+        
+        // FORMA NOVA:
+        aplicarAjustesMaster(avgDiff);
+        
+        // O Master envia os ajustes para cada Slave individualmente
+        enviarAjustes(socket, avgDiff);
+    }
+
+    private void aplicarAjustesMaster(int adjustment) {
+        int oldTime = horario;
+        horario += adjustment;
+        System.out.println("Master ajustou horário em " + adjustment + ". Horário antigo: " + oldTime + ", Novo horário: " + horario);
+    }
+
+    // Para cada slave, o Master calcula o ajuste e envia a mensagem com a informação
+    private void enviarAjustes(MulticastSocket socket, int avgDiff) throws Exception {
+        for (Map.Entry<InetSocketAddress, Integer> entry : listaDifSlaves.entrySet()) {
+            InetSocketAddress address = entry.getKey();
+            if (address.getPort() == MULTICAST_PORT) { continue; }
+            int clientDiff = entry.getValue();
+            int adjustment = calcularAjuste(clientDiff, avgDiff);
+            String msg = "AJUSTE:" + adjustment;
+            NetworkUtils.sendPacket(socket, msg.getBytes(), address);
+            System.out.println("Enviou ajuste " + adjustment + " para o Slave em " + address);
+        }
+    }
+
+    public static int calcularDiferencaMedia(Map<InetSocketAddress, Integer> clientDiffs) {
+        long sumDiff = 0;
+        int count = 0;
+        for (Integer diff : clientDiffs.values()) {
+            sumDiff += diff;
+            count++;
+        }
+        return (int) (sumDiff / count);
+    }
+
+    public static int calcularAjuste(int clientDiff, int avgDiff) {
+        return -(clientDiff - avgDiff);
+    }
+
+    /* COMENTÁRIO ALANA -> O MÉTODO calcularAjusteMaster PARECE INÚTIL, JÁ QUE ELE SÓ RETORNA O QUE JÁ PASSAMOS COMO PARÂMETRO
+     * public static int calcularAjusteMaster(int avgDiff) {
+        return avgDiff;
+    }*/
+}
---
+++ Trabalho02SD/src/trabalho02/Slave.java
@@ -0,0 +1,130 @@
+package trabalho02;
+
+import java.net.*;
+
+public class Slave implements Runnable {
+	private int horario;
+	private final String ip;
+	private final int porta;
+    private static final int MULTICAST_PORT = 8000;
+    private static final String MULTICAST_GROUP = "228.5.6.7";
+    private static final int TIMEOUT = 15000;
+
+	public Slave(int horario, String ip, int porta) {
+		this.horario = horario;
+		this.ip = ip;
+		this.porta = porta;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public int getPorta() {
+		return porta;
+	}
+
+    @Override
+    public void run() {
+        System.out.println("Rodando Slave em " + porta + ", clock: " + horario);
+        escutarMaster();
+    }
+
+	public void escutarMaster() {
+        MulticastSocket multicastSocket = null;
+        DatagramSocket unicastSocket = null;
+        try {
+        	// O Slave cria o socket multicast e entra no grupo
+            multicastSocket = criarMulticastSocket();
+            InetAddress group = InetAddress.getByName(MULTICAST_GROUP);
+            multicastSocket.joinGroup(group);
+
+            // O Slave cria um socket unicast na porta definida e inicia uma nova thread para ouvir os ajustes que o Master enviar
+            unicastSocket = new DatagramSocket(porta);
+
+            DatagramSocket finalUnicastSocket = unicastSocket;
+            new Thread(() -> escutarUnicast(finalUnicastSocket)).start();
+
+            while (true) {
+                processarMensagensMulticast(multicastSocket, group);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (multicastSocket != null) {
+                try {
+                    multicastSocket.leaveGroup(InetAddress.getByName(MULTICAST_GROUP));
+                    multicastSocket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (unicastSocket != null) {
+                unicastSocket.close();
+            }
+        }
+	}
+
+    private MulticastSocket criarMulticastSocket() throws Exception {
+        MulticastSocket socket = new MulticastSocket(MULTICAST_PORT);
+        socket.setSoTimeout(TIMEOUT);
+        return socket;
+    }
+
+    // O Slave fica ouvindo mensagens de AJUSTE enviadas pelo Master, e caso seja uma dessas, realiza o ajuste
+    private void escutarUnicast(DatagramSocket socket) {
+        while (true) {
+            try {
+                DatagramPacket packet = NetworkUtils.receivePacket(socket);
+                String message = new String(packet.getData(), 0, packet.getLength());
+                if (message.startsWith("AJUSTE")) {
+                    processarAjuste(message);
+                }
+            } catch (SocketTimeoutException e) {
+                System.out.println("Slave na porta " + porta + ": Nenhuma mensagem multicast recebida.");
+            } catch (Exception e) {
+                System.out.println("Slave na porta " + porta + " erro ao processar mensagem multicast: " + e.getMessage());
+            }
+        }
+    }
+    
+    private void processarMensagensMulticast(MulticastSocket socket, InetAddress group) throws Exception {
+        try {
+        	// O Slave recebe mensagens via multicast
+            DatagramPacket packet = NetworkUtils.receivePacket(socket);
+            String message = new String(packet.getData(), 0, packet.getLength());
+            
+            // Se for o horário do master, processa
+            if (message.startsWith("HORARIO_MASTER")) {
+                processarHorarioMaster(socket, group, message);
+            }
+        } catch (SocketTimeoutException e) {
+            System.out.println("Slave na porta " + porta + ": Nenhuma mensagem multicast recebida.");
+        } catch (Exception e) {
+            System.out.println("Slave na porta " + porta + " erro ao processar mensagem multicast: " + e.getMessage());
+        }
+    }
+
+    // Pega o horário enviado pelo master, calcula a diferença e envia a mensagem com a informação de volta
+    private void processarHorarioMaster(MulticastSocket socket, InetAddress group, String message) throws Exception {
+        int masterTime = Integer.parseInt(message.split(":")[1]);
+        System.out.println("Slave na porta " + porta + " recebeu horário do Master: " + masterTime);
+        
+        int diff = calcularDiferenca(masterTime);
+        String sDiff = "DIF:" + diff + ":" + getIp() + ":" + getPorta();
+        NetworkUtils.sendMulticastPacket(socket, sDiff.getBytes(), group, MULTICAST_PORT);
+    }
+
+    // O Slave aplica o ajuste recebido na mensagem do Master
+    private void processarAjuste(String message) {
+        int adjustment = Integer.parseInt(message.split(":")[1]);
+        int oldTime = horario;
+        horario += adjustment;
+        System.out.println("Slave na porta " + porta + " ajustou horário em " + adjustment +
+                ". Horário antigo: " + oldTime + ", Novo horário: " + horario);
+    }
+
+	private int calcularDiferenca(int horarioMaster) {
+		return horario - horarioMaster;
+	}
+}
